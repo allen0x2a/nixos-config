f2523887c9215af072a83f37ce29c11c7d3fd031
allen0x2a <248719143+allen0x2a@users.noreply.github.com>
2026-02-22 13:15:02 -0500

    Update networking.nix
    reducing boot-up time

--- system/networking.nix
+++ system/networking.nix
@@ -1,7 +1,15 @@
+# networking.nix
 { config, pkgs, ... }:
 {
   networking.hostName = "nixos";
   networking.networkmanager.enable = true;
+
+  # Set to flase because redundant with NetworkManager
+  networking.dhcpcd.enable = false;
+
+  # Set to false otherwise will block the boot/login if the KVM network isn't online
+  systemd.services.NetworkManager-wait-online.enable = false;
+
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
