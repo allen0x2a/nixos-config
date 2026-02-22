6cc6f490851af75bb1c16e88f9025963cb11f452
allen0x2a <248719143+allen0x2a@users.noreply.github.com>
2026-02-22 13:33:06 -0500

    Update nvidia.nix

--- system/nvidia.nix
+++ system/nvidia.nix
@@ -1,5 +1,7 @@
+# nvidia.nix
 { config, pkgs, ... }:
 {
+  # NVIDIA proprietary driver for the X server.
   services.xserver.videoDrivers = ["nvidia"];
 
   hardware.nvidia = {
@@ -10,9 +12,20 @@
     package = config.boot.kernelPackages.nvidiaPackages.stable;
   };
 
-  # reduce DRM errors
+  # Reduce DRM errors and monitor(s) freezing
+  # - modeset=1: Enable kernel modesetting for proper display management
+  # - fbdev=1: Enable framebuffer device, fixes dual monitor freeze during login
+  # - PreserveVideoMemoryAllocations=1: Preserve VRAM on suspend/resume
+  # - NVreg_OpenRmEnableUnsupportedGpus=1 Explicitly allow the Open RM for the 5080 to prevent init hangs
+  # - pci=noaer Explicitly allow the Open RM for the 5080 to prevent init hangs
+  # - efifb and vesafb=off: Disable framebuffers to prevent the kernel from 'hanging' while searching for monitors on the secondary GPU
   boot.kernelParams = [
     "nvidia-drm.modeset=1"
+    "nvidia-drm.fbdev=1"
     "nvidia.NVreg_PreserveVideoMemoryAllocations=1"
+    "nvidia.NVreg_OpenRmEnableUnsupportedGpus=1"
+    "pci=noaer"
+    "video=efifb:off"
+    "video=vesafb:off"
   ];
 }
